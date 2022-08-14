c164829b846e327bc453b9110d0d94571533ef6e
Ankit-11525 <Ankitpathal11525@gmail.com>
2022-08-14 18:35:05 +0530

    added scroll feature to Talking Therapy

---
+++ js/scrollsmooth.js
@@ -0,0 +1,18 @@
+const scroll_button=document.getElementsByClassName(".smooth-scroll");
+for(let i of scroll_button)
+{
+    i.addEventListener('click',function(e){
+        e.preventDefault();
+        const target=this.getAttribute('href');
+        const target_position=document.querySelector(target);
+        console.log(target_position)
+        target_position.scrollIntoView({scrollTop:target.scrollTop,behavior:'smooth'}); 
+      
+       
+
+
+
+        
+       
+    });
+}
