2a926b8a4335a0369c552c9fb28f1d0ec29baeb8
Vandana <vp317470@gmail.com>
2022-08-24 15:35:50 +0530

    Update script.js
    Signed-off-by: Vandana <vp317470@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        center: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
+
+
 const countdown = () => {
   const countDate = new Date("May 21, 2022 00:00:00").getTime();
   const now = new Date().getTime();
