4e5e7fe09733908a33e5b72396baa59427875f6e
Anshu Pathak <78332454+AnshuPathak-88825@users.noreply.github.com>
2022-09-11 20:12:12 +0530

    updated smooth-scroll
    Signed-off-by: Anshu Pathak <78332454+AnshuPathak-88825@users.noreply.github.com>

--- js/scrollsmooth.js
+++ js/scrollsmooth.js
@@ -1,18 +1,10 @@
-const scroll_button=document.getElementsByClassName(".smooth-scroll");
+const scroll_button = document.getElementsByClassName(".smooth-scroll");
 for(let i of scroll_button)
 {
     i.addEventListener('click',function(e){
         e.preventDefault();
-        const target=this.getAttribute('href');
-        const target_position=document.querySelector(target);
-        console.log(target_position)
+        const target = this.getAttribute('href');
+        const target_position = document.querySelector(".target");
         target_position.scrollIntoView({scrollTop:target.scrollTop,behavior:'smooth'}); 
-
-
-
-
-
-
-
     });
 } 
