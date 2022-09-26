4560c7af4e99f97ac5d17759cfbe95a371d4dad2
ayushkumar <aaayushsinghh@gmail.com>
2022-09-26 10:24:02 +0530

    yoga page

---
+++ javascript/yoga.js
@@ -0,0 +1,14 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
